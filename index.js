0f57be91b195c9e61ec88d3ff86a70c0fa12bc7f
Matias Garcia <matiasigarcia91@gmail.com>
2022-01-03 14:54:20 +0100

    space router and simple endpoints

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const corsMiddleWare = require("cors");
 const { PORT } = require("./config/constants");
 const authRouter = require("./routers/auth");
 const authMiddleWare = require("./auth/middleware");
+const spacesRouter = require("./routers/spaces");
 
 const app = express();
 const cors = corsMiddleWare();
@@ -124,6 +125,7 @@ app.post("/authorized_post_request", authMiddleWare, (req, res) => {
 });
 
 app.use("/", authRouter);
+app.use("/spaces", spacesRouter);
 
 // Listen for connections on specified port (default is port 4000)
 
---
+++ routers/spaces.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const Space = require("../models").space;
+const Story = require("../models").story;
+
+const router = new Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const spaces = await Space.findAll({ include: [Story] });
+    res.send(spaces);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:spaceId", async (req, res, next) => {
+  try {
+    const spaceId = req.params.spaceId;
+    const spaces = await Space.findByPk(spaceId, { include: [Story] });
+    res.send(spaces);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
